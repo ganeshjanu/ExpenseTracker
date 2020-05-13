46a4aff25942b08e4328df8f1f1702b74f8bd00b
Ganeshjanu <ganeshjanu@gmail.com>
2020-05-13 12:44:37 -0400

    cleanedup

--- PaymentService/src/main/java/com/friends/payment/exceptions/RepositoryException.java
+++
@@ -1,9 +0,0 @@
-package com.friends.payment.exceptions;
-
-public class RepositoryException extends Exception {
-
-	public RepositoryException(String message) {
-		super(message);
-	}
-
-}
--- PaymentService/src/main/java/com/friends/payment/repository/PaymentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.friends.payment.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import com.friends.payment.beans.Payment;
-
-@Repository
-public interface PaymentRepository extends JpaRepository<Payment, String> {}
