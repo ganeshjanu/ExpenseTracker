ce0308d3f14ff5ede0f8fa03ffa06d0b8bbcb4ca
shail <shail@DESKTOP-9V2EI0M>
2020-05-17 18:46:53 -0400

    test git pipeline auto build

--- employee/src/main/java/com/friends/employee/EmployeeApplication.java
+++ employee/src/main/java/com/friends/employee/EmployeeApplication.java
@@ -3,6 +3,7 @@ package com.friends.employee;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//test jenkin build
 @SpringBootApplication
 public class EmployeeApplication {
 
